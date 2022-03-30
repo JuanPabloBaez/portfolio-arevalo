3a8029077fb3fcdb639c27aa879254ef5adc6212
JP Baez <baezjuanpablo@gmail.com>
2022-03-30 16:05:09 +0200

    react player

--- src/components/home.js
+++ src/components/home.js
@@ -2,7 +2,8 @@ import React, { useEffect } from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { EffectFade, Autoplay, Navigation} from "swiper";
 import {useRecoilState, useRecoilValue} from 'recoil';
-import {darkState, imgState, conciertoState, prensaState} from '../App.js'
+import {darkState, imgState, conciertoState, prensaState} from '../App.js';
+import ReactPlayer from 'react-player/lazy';
 import 'swiper/css';
 import "swiper/css/effect-fade";
 import HomeLogo from "../assets/logo-home-white.svg";
@@ -10,7 +11,7 @@ import MailLogo from "../assets/icons/email.svg";
 import FacebookLogo from "../assets/icons/facebook.svg";
 import InstaLogo from "../assets/icons/instagram.svg";
 import SpotifyLogo from "../assets/icons/spotify.svg";
-import YoutubeLogo from "../assets/icons/youtube.svg";
+
 
 
 function Home() {
@@ -29,6 +30,25 @@ function Home() {
       <header>
         <div className='home-first'>
           <img className='home-logo' src={HomeLogo} alt="logo de Alejandro Arévalo"/>
+          <ReactPlayer 
+            url='https://soundcloud.com/alejandro-javier-ar-valo-berrios/sets/el-sueno-y-las-ruinas?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing'
+            className='react-player'
+            light= 'true'
+            width={"40%"}
+            height={"20vh"}
+            background-image={"none"}
+            
+            config={{
+              soundcloud:{
+                options:{
+                  show_artwork: false,
+                  show_user: false,
+                  color: "#038cfc",
+                }
+              } 
+            }}
+           />
+
         </div>
         <Swiper
         spaceBetween={20}
@@ -56,7 +76,6 @@ function Home() {
             <a href="https://open.spotify.com/artist/3LJFVzMZewx7BoG5Cuh4N5?si=i3W01HiIToepWX5Mnl4-4Q" target="_blank"  rel="noreferrer" ><img src={SpotifyLogo} alt="Spotify de Alejandro Arévalo"/></a>
             <a href="https://www.instagram.com/alejandro_arevalo_pianista/" target="_blank"  rel="noreferrer"><img src={InstaLogo} alt="Instagram de Alejandro Arévalo"/></a>
             <a href="https://www.facebook.com/AlejandroArevaloMusico" target="_blank"  rel="noreferrer"><img src={FacebookLogo} alt="Facebook de Alejandro Arévalo"/></a>
-            <a href="https://www.youtube.com/user/AlejandroArevalob" target="_blank"  rel="noreferrer"><img src={YoutubeLogo} alt="Youtube de Alejandro Arévalo"/></a>
         </div>
         <div className='prox-conciertos'>
           <h2>Próximos Conciertos</h2>
